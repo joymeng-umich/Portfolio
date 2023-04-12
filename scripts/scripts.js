ef533cc2e2f810b007f7f28960094a6a5c5e877a
joymeng-umich <116969883+joymeng-umich@users.noreply.github.com>
2023-04-12 01:09:31 -0400

    finished about

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -27,3 +27,10 @@ document.addEventListener('DOMContentLoaded', function () {
         progressBar.value = window.scrollY;
   });
 });
+
+//back to top
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
