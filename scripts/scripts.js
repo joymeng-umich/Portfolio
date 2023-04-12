5c16153234b93996c4b1996844ff42256f9b40ea
joymeng-umich <116969883+joymeng-umich@users.noreply.github.com>
2023-04-12 03:57:42 -0400

    trying scroll snap

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,10 +6,11 @@
 
 window.onscroll = function() {
     let Scrollpos = window.pageYOffset;
-  if (Scrollpos < 600) {
+    // console.log(Scrollpos)
+  if (Scrollpos < 60) {
     document.querySelector("header").style.top = "0";
   } else {
-    document.querySelector("header").style.top = "-500px";
+    document.querySelector("header").style.top = "-4rem";
   }
 }
 
@@ -19,18 +20,24 @@ document.addEventListener('DOMContentLoaded', function () {
   var winHeight = window.innerHeight,
         docHeight = document.documentElement.scrollHeight,
         progressBar = document.querySelector('#content_progress');
-  progressBar.max = docHeight - winHeight;
+  if (progressBar) {
+    progressBar.max = docHeight - winHeight;
   progressBar.value = window.scrollY;
 
   document.addEventListener('scroll', function () {
         progressBar.max = document.documentElement.scrollHeight - window.innerHeight;
         progressBar.value = window.scrollY;
   });
+
+  }
+  
 });
 
 //back to top
 
 function topFunction() {
+  // document.querySelector("header").style.top = "0";
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  
 }
